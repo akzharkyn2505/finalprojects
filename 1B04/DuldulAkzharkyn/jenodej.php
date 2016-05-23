4ea746079b73c232d61995fa885b0826318a7e2e
akzharkyn2505 <zhara2505@mail.ru>
2016-05-23 12:25:08 +0600

    Create jenodej.php

---
+++ 1B04/DuldulAkzharkyn/jenodej.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MARAIZ.kz</title>
+<script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
+<script type="text/javascript" src="js/gallery.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script language="javascript" type="text/javascript"></script>
+
+<div class="main">
+
+<?php 
+include("header.php");
+?>
+	<div class="slideshowmenu">	
+<div id="gallery">
+
+	<a href="#" class="show">
+		<img src="img/main/jen1.png" alt="Cinque" width="703" height="500" title="" alt="" rel="<h3>Cinque</h3>Бренд Cinque успешно представлен в Германии более, чем в 1000 магазинах и торговых сетях , преимущественно в высоком ценовом сегменте, а также в 300 магазинах по всему миру "/>
+	</a>
+	
+	<a href="#">
+		<img src="img/main/jen2.png" alt="REPLAY" width="703" height="500" title="" alt="" rel="<h3>REPLAY </h3>Forefront of Italian fashion since 1978, Replay designer clothing continues to be a significant presence within the fashion circuit. With a modern attitude and multiple inspirations, their innovative denim design and contemporary street style make sure Replay clothing is a firm favourite with the fashion conscious trend setter.c
+ "/>
+	</a>
+	
+	<a href="#">
+		<img src="img/main/jen3.png" alt="PEPE jeans " width="703" height="500" title="" alt="" rel="<h3>PEPE jeans </h3>Pepe Jeans London is a denim and casual wear jeans brand that was established in the Portobello Road area of London in 1973. From its origins as a tiny market stall to more than half a US billion dollar brand, Pepe has transformed itself into a jeanswear label found throughout Europe. The brand today has presence in more than 80 countries across the world. "/>
+	</a>
+
+	
+
+
+	<div class="caption"><div class="content"></div></div>
+</div>
+<div class="clear"></div></div>
+
+	<br><br>
+
+	<div class="thumbmenu">
+   		<a href="sumki.php"><img src="img/footerpic/jenodej1.jpg" alt="Фотография 1" width="230" height="190" ></a>
+   		<a href="tufli.php"><img src="img/footerpic/jenodej2.jpg" alt="Фотография 2" width="230" height="190"></a>
+   		<a href="sumki.php"><img src="img/footerpic/jenodej3.jpg" alt="Фотография 3" width="230" height="190"></a>
+    </div>
+	
+	
+	</div>
+	
+	<?php 
+	include("footer.php");
+	?>
+	<div class="jenm">
+<ul>
+<h3>Женская одежда</h3>
+<li class="jeno"><a href="bluzy.php">Блузы</a></li>
+<li class="jeno"><a href="kapri.php">Бриджи и капри</a></li>
+<li class="jeno"><a href="briuki.php">Брюки</a></li>
+<li class="jeno"><a href="gol.php">Головные уборы</a></li>
+<li class="jeno"><a href="jenodej.php">Джемперы</a></li>
+<li class="jeno"><a href="djinsy.php">Джинсы</a></li>
+<li class="jeno"><a href="jilety.php">Жилеты</a></li>
+<li class="jeno"><a href="jenodej.php">Кардиганы</a></li>
+<li class="jeno"><a href="jenodej.php">Комбинезоны</a></li>
+<li class="jeno"><a href="jenodej.php">Костюмы спортивные</a></li>
+<li class="jeno"><a href="losiny.php">Лосины</a></li>
+<li class="jeno"><a href="platya.php">Платья</a></li>
+<li class="jeno"><a href="jenodej.php">Туники</a></li>
+<li class="jeno"><a href="jenodej.php">Футболки</a></li>
+<li class="jeno"><a href="iubki.php">Юбки</a></li>
+<li class="jeno"><a href="worty.php">Шорты</a></li>
+</ul>
+</div>
+<div id="sozder">
+	<h4>Бутик.ру – интернет магазин одежды и сумок!</h4>
+	<p>Модно и стильно одеваться любят все, но ходить по магазинам, оказывается, нравится не каждому. Утомительная беготня из одного магазина в другой, наличие длинных очередей, нехватка времени для примерки, постоянный стресс – все это превращает, казалось бы, приятное желание приобрести одежду, сумку или обувь в сплошной кошмар. <BR>Купить одежду в Интернет магазине Бутик очень легко - не нужно тратить время на передвижение по городу и совершать покупки «впопыхах». <BR>В нашем онлайн магазине шопинг, действительно, превращается в удовольствие: Вы комфортно устраиваетесь перед монитором и начинаете свое путешествие.</p>
+	<h4>MARAIZ.kz - проверенный временем интернет-магазин одежды и сумок с заслуженной репутацией</h4>
+	<p>На протяжении вот уже 10 лет мы успешно сражаемся за доверие наших любимых покупателей. 
+Мы постоянно расширяем ассортимент одежды и сумок новыми брендами и, самое главное, в магазине MARAIZ.kz Вы никогда не найдете подделок и стоковых вещей из старых коллекций.
+Покупая одежду или сумку в интернет магазине Бутик.ру, Вы на 100% можете быть уверены в том, что это действительно оригинальная дизайнерская вещь из последней коллекции модного бренда: NO FAKE! NO STOCK! - магазин одежды и сумок Бутик.ру не продает копий, подделок и стоковых вещей.</p>
+	<h4>Женская и мужская одежда и сумки в интернет магазине MARAIZ.kz</h4>
+	<p>Если вы относите себя к людям, которые любят красиво и стильно одеваться, но длительные походы в магазин Вас утомляют, то вы попали туда, куда нужно. <BR>Сайт MARAIZ.kz представляет собой подробный интернет-каталог с множеством разделов: женская и мужская одежда, обувь, аксессуары, сумки и портфели. <BR><STRONG>Раздел женской одежды</STRONG> в магазине MARAIZ представляет широкий ассортимент одежды для женщин разных моделей, размеров, соотношения цены и качества. Здесь вы сможете купить или заказать нужную вам одежду через интернет и увидеть, что сегодня предлагают покупателям известные в мире моды бренды. <BR><STRONG>Раздел мужской одежды</STRONG> в нашем магазине предлагает обратить внимание на широкий ассортимент одежды, начиная от покупки красивых носков до выбора пиджака или куртки, которые предлагают для мужчин ведущие мировые производители. <BR>Отдельный раздел интернет магазина MARAIZ.kz представляет <STRONG>мужские и женские сумки</STRONG>. У нас огромный ассортимент модных мужских и женских сумок, в котором каждый сможет выбрать и купить сумку, подчеркивающую его индивидуальность. <BR>В магазине сумок Бутик.ру представлено множество известных итальянских, английских, американских и других брендов мужских и женских сумок.</p>
+	<h4> В России MARAIZ.kz редставлен под названием BUTIK.RU </h4>
+	<p>Наш интернет-бутик брендовой одежды предлагает своим клиентам услуги службы доставки, которая привезет купленную вами вещь в любую точку России. При возникновении вопросов всегда можно обратиться в нашу службу поддержки или оставить свои вопросы и отзывы по работе магазина на сайте или уточнить наличие нужного размера. 
+Кроме того, возможность совершить покупку через Интернет, воспользовавшись нашей службой доставки, не является единственной. Для любителей традиционного шопинга покупку можно совершить, посетив наш магазин в Москве, располагающийся по адресу: м.Новокузнецкая, Большой Овчинниковский переулок, д.16, ТЦ «Аркадия», 2-й этаж.</p>
+    </div> 
+</body>
+</html>
