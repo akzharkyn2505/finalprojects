006fd1985dfb082fd276b73f8b336093efd5583e
akzharkyn2505 <zhara2505@mail.ru>
2016-05-23 12:16:41 +0600

    Create header.php

---
+++ 1B04/DuldulAkzharkyn/header.php
@@ -0,0 +1,33 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<div class="logo"><a href="index.php"><img src="img/mar.kz.gif" alt="" title="" border="0" /></a> </div>  
+  
+<div class="logo1"><a href="register.php"><img src="img/register.png" alt="" title="" border="0" /></a> </div> 
+ 
+ <div class="logo2"><a href="myaccount.php"><img src="img/vhod.png" alt="" title="" border="0" /></a> </div>
+<div id="menu_search">
+        <div id="menu">
+	        <ul>
+                    <li><a href="jenodej.php"> Женская <br> одежда</a></li>
+       		 		<li><a href="mujodej.php">Мужская <br>одежда</a></li>
+           		 	<li><a href="jenaks.php">Женские <br> аксессуары</a></li>
+           			<li><a href="mujaks.php">Мужские <br> аксессуары</a></li>
+            		<li><a href="sumki.php"> Сумки <br> Портфели</a></li>
+            		<li><a href="sale.php">Распродажа</a></li>
+			<li>
+			<div id="search_section">
+            <form action="getproduct.php" method="get">
+              <input type="text" name="request" size="21" id="input" onkeyup='search()' ; />
+              <input type="image" class='button' alt="Submit button"/>
+			</form>
+			<div id="txtHint"></div>
+		</div>
+
+		</li>
+		
+		<li><div id="korzina"> <a href="index.php"><img src="img/basket.jpg"></a></div></li>	
+            </ul>    	
+			</div>
+			</div>
+      </html>
