a13a655bdce379f930fe6f24ba3d36688b7147f6
akzharkyn2505 <zhara2505@mail.ru>
2016-05-23 12:20:55 +0600

    Create sale.php

---
+++ 1B04/DuldulAkzharkyn/sale.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Скидки</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<head>
+<script src="scripts/mootools.v1.11.js" type="text/javascript"> </script>
+<script src="scripts/jd.gallery.js" type="text/javascript"> </script>
+<?php 
+include("header.php");
+?>
+</head>
+<body>
+
+<div class="sale"><a href="sale.php"><img src="img/main/sale.png" alt="" title="" border="0" /></a> </div>
+
+<div class="jenm">
+<ul>
+<h2>Скидки до 70%</h2>
+<h3>Утоваренные товары</h3>
+<li class="jeno"><a href="jenodej.php">женская одежда</a></li>
+<li class="jeno"><a href="jenodej.php">мужская одежда</a></li>
+<li class="jeno"><a href="briuki.php">аксессуары</a></li>
+
+<h3>Прошлый сезон</h3>
+<li class="jeno"><a href="jenodej.php">Вещи в тренде</a></li>
+<li class="jeno"><a href="jenodej.php">Максимально удобные</a></li>
+<li class="jeno"><a href="jenodej.php">Стиль и мода</a></li>
+
+</ul>
+
+
+</div>
+
+<?php 
+include("footer.php");
+?>
+
+</body>
+</html>
+
